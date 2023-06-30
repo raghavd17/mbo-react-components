af4557caf75112dd615743cc9249043c82116bee
Raghavendra Diddimani <rdiddimani@mbopartners.com>
2023-06-30 16:14:34 +0530

    label component updated with all varints and css modules

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -12,11 +12,11 @@ const Nav = () => {
       <NavLink to={'/List'} key={2} className="border-b-2  border-primary-300"> List </NavLink>
       <NavLink to={'/Dropdown'} key={3}className="border-b-2 border-primary-300"> Dropdown </NavLink>
       <NavLink to={'/Date'} key={4} className="border-b-2 border-primary-300"> Date </NavLink>
-      {/* <NavLink to={'/Label'} key={5} className="border-b-2 border-primary-300"> Label </NavLink> */}
-      <NavLink to={'/Search'} key={5} className="border-b-2 border-primary-300"> Search </NavLink>
-      <NavLink to={'/InputRadio'} key={6} className="border-b-2 border-primary-300"> Input Radio </NavLink>
-      <NavLink to={'/InputCheckbox'} key={7} className="border-b-2 border-primary-300"> Input Checkbox </NavLink>
-      <NavLink to={'/Avatar'} key={8} className="border-b-2 border-primary-300"> Avatar </NavLink>
+      <NavLink to={'/Label'} key={5} className="border-b-2 border-primary-300"> Label </NavLink>
+      <NavLink to={'/Search'} key={6} className="border-b-2 border-primary-300"> Search </NavLink>
+      <NavLink to={'/InputRadio'} key={7} className="border-b-2 border-primary-300"> Input Radio </NavLink>
+      <NavLink to={'/InputCheckbox'} key={8} className="border-b-2 border-primary-300"> Input Checkbox </NavLink>
+      <NavLink to={'/Avatar'} key={9} className="border-b-2 border-primary-300"> Avatar </NavLink>
     </ul>
     <Outlet />
   </nav>
